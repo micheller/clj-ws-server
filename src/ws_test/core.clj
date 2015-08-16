4cd04368c32d6640880127ceb60b30bab549a4cc
Michael Koltsov <mikekoltsov@gmail.com>
2015-08-17 00:48:26 +0300

    console logging and style fixes

--- src/ws_test/core.clj
+++ src/ws_test/core.clj
@@ -2,32 +2,29 @@
   (:gen-class)
   (:use org.httpkit.server
         ring.middleware.reload
-        ;; [clojure.edn :only [pr-str]]
-        [clojure.data.json :only [json-str read-json]])
-)
+        [clojure.data.json :only [json-str read-json]]))
 
 ;; (defn app [req]
 ;;   {:status  200
 ;;    :headers {"Content-Type" "text/html"}
-;;    :body    "hello HOOE!"})
+;;    :body    "hello, darling!"})
 
 (defn answer []
-  ;; (pr-str ["NYET" {:nyet "NYET" :once-more {:NYET "NYET"}}])
-  (json-str {:sorry "We're out of dicks" :dish-of-the-day "Vaginas"})
-  )
+  (pr-str {:sorry "We're out of the dicks you ordered"
+           :dish-of-the-day "chocolate caviar fountain with bacon crackers"}))
 
 (defn handler [request]
   (with-channel request channel
-    (send! channel "EHLO")
-    ;; (println request)
-    (on-close channel (fn [status] (println "channel closed: " status)))
-    (on-receive channel (fn [data] ;; echo it back
-                          (print "---> ")
-                          (println data)
-                          (print "<--- ")
-                          (println (answer) "\n")
-                          (send! channel (answer))))
-))
+    ;; (send! channel "EHLO")
+    (println request)
+    (on-close channel (fn [status] (println "channel closed: " status "\n")))
+    (on-receive channel (fn [data]
+                          (let [ans (answer)]
+                            (print "---> ")
+                            (println data)
+                            (print "<--- ")
+                            (println ans "\n")
+                            (send! channel ans))))))
 
 (defn -main [& args]
   (run-server (wrap-reload #'handler) {:port 8088}))
